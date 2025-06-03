4611d42f3a78cf4adff58b3e9503a1f94ee75d59
Mengfei Zhang <mengfeiz@usc.edu>
2025-06-03 14:01:18 +0800

    add site title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Fiona Zhang Website',
+  description: 'Personal website of Fiona Zhang',
+  generator: 'Fiona Zhang',
 }
 
 export default function RootLayout({
